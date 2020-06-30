295afa492884b9d89d4077f7578ed06259765d9b
syuka217 <tomoki1236@gmail.com>
2020-06-30 16:29:36 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  
+   def top 
+   end
+
+   def create
+   	@book = Book.new(book_params)
+
+   	if @book.save
+      flash[:notice] = "successfully"
+   		redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render action: :index
+   	end
+
+   end
+
+   def index
+   	@books = Book.all
+    @book = Book.new
+   end
+
+   def show
+    @book = Book.find(params[:id])
+   end
+   def edit
+        @book = Book.find(params[:id])
+   end
+   def update
+       @book = Book.find(params[:id])
+
+       if @book.update(book_params)
+       flash[:notice] = "successfully"
+       redirect_to book_path(@book.id)
+       else
+       render action: :edit
+       end
+   end
+   def destroy
+        book = Book.find(params[:id]) #データ(レコード)を1件取得
+        book.destroy #データ（レコード）を削除
+        redirect_to books_path #List一覧画面へリダイレクト
+   end
+
+   private
+   
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+ 
+   	
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+	root 'books#top'
+
+	resources :books
+
+
+end
